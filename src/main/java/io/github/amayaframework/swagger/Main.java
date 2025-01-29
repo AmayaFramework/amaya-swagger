87a60351928b0026bae2b10b2b9d5d326f1cfe45
Roman Bakaldin <gbakaldin@gmail.com>
2025-01-29 17:43:01 +0300

    Update example main and libs

--- src/main/java/io/github/amayaframework/swagger/Main.java
+++ src/main/java/io/github/amayaframework/swagger/Main.java
@@ -1,15 +1,26 @@
 package io.github.amayaframework.swagger;
 
 import io.github.amayaframework.core.WebBuilders;
+import io.github.amayaframework.http.HttpCode;
+import io.github.amayaframework.http.MimeData;
+import io.github.amayaframework.http.MimeType;
 import io.github.amayaframework.jetty.JettyServerFactory;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.github.amayaframework.openui.ApiEntry;
+import io.github.amayaframework.swaggerui.SwaggerUIFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.FileInputStream;
+import java.net.URI;
 
 public class Main {
-    static final String URL_ENTRY_TEMPLATE = "{url:\"%url\",name:\"%name\"}";
+    private static final MimeType YAML = new MimeType("text", "yaml", true);
+
+    public static URI getURI(URI root, String swagger) {
+        var uri = URI.create(swagger).normalize();
+        if (uri.isAbsolute()) {
+            return uri;
+        }
+        return URI.create(root.getPath() + "/" + uri.getPath());
+    }
 
     public static void main(String[] args) throws Throwable {
         var builder = WebBuilders.create();
@@ -17,35 +28,47 @@ public class Main {
                 .setServerFactory(new JettyServerFactory())
                 .build();
         app.bind(8080);
+        // Prepare swagger
+        var root = URI.create("/static/root");
+        var swagger1 = getURI(root, "./swagger.json");
+        var swagger2 = getURI(root, "https://petstore.swagger.io/v2/swagger.json");
+        var uif = new SwaggerUIFactory();
+        var ui = uif.create(
+                root,
+                ApiEntry.of(swagger1, "Petstore1"),
+                ApiEntry.of(swagger2, "Petstore2")
+        );
         app.run(ctx -> {
             var rq = ctx.getRequest();
-            var os = ctx.getResponse().getOutputStream();
-            var seg = rq.getPathSegments().get(0);
-            if (seg.equals("/")) {
-                seg = "index.html";
+            var rp = ctx.getResponse();
+            System.out.println(rq.getPath());
+            var uri = rq.getRequestURI();
+            var comp = root.relativize(uri);
+            var path = comp.getPath();
+            if (path.equals("/")) {
+                path = ui.getIndex();
             }
-            writeToRsp(seg, os);
+            if (path.equals("swagger.json")) {
+                rp.setMimeType(MimeType.JSON);
+                var is = new FileInputStream("swagger.json");
+                is.transferTo(rp.getOutputStream());
+                is.close();
+                return;
+            }
+            if (path.equals("swagger.yaml")) {
+                rp.setMimeData(MimeData.of(YAML, "charset", "utf-8"));
+                var is = new FileInputStream("swagger.yaml");
+                is.transferTo(rp.getOutputStream());
+                is.close();
+                return;
+            }
+            var found = ui.getInputStream(path);
+            if (found == null) {
+                rp.sendError(HttpCode.NOT_FOUND, "File " + path + " not found");
+                return;
+            }
+            found.transferTo(rp.getOutputStream());
+            found.close();
         });
-    }
-
-    static void writeToRsp(String name, OutputStream os) throws IOException {
-        System.out.println("GETTING " + name);
-        var rsc = Main.class.getResourceAsStream(name);
-        if (rsc == null) {
-            os.write("NOT FOUND!\n".getBytes());
-            return;
-        }
-        rsc.transferTo(os);
-        rsc.close();
-    }
-
-    interface ApiSerializer {
-
-        void serialize(OpenAPI api, OutputStream ous);
-    }
-
-    interface ApiDeserializer {
-
-        OpenAPI deserialize(InputStream is);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,15 @@
 module io.github.amayaframework.swagger {
     // TODO
     requires static io.swagger.v3.oas.models;
+    requires io.github.amayaframework.openui;
+    requires io.github.amayaframework.swaggerui;
     // Imports
     requires io.github.amayaframework.core;
     requires io.github.amayaframework.web;
     requires io.github.amayaframework.jetty;
     requires com.github.romanqed.jfunc;
     requires io.github.amayaframework.context;
+    requires io.github.amayaframework.http;
     // Exports
     exports io.github.amayaframework.swagger;
 }
