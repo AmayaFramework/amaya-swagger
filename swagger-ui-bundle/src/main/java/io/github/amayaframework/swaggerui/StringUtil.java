d0857a365ca26aadc8c2657af2da6c484e0ce171
Roman Bakaldin <gbakaldin@gmail.com>
2025-08-21 17:03:13 +0300

    Fix StringUtil

--- swagger-ui-bundle/src/main/java/io/github/amayaframework/swaggerui/StringUtil.java
+++ swagger-ui-bundle/src/main/java/io/github/amayaframework/swaggerui/StringUtil.java
@@ -9,6 +9,7 @@ final class StringUtil {
     }
 
     static final String EMPTY_URLS = "urls:[]";
+    static final String URLS_PREFIX = "urls:[";
     static final String MARKER = "//%";
     static final String INIT_JS = "window.onload=function(){window.ui=SwaggerUIBundle({//%,dom_id:\"#swagger-ui\",deepLinking:!0,presets:[SwaggerUIBundle.presets.apis,SwaggerUIStandalonePreset],plugins:[SwaggerUIBundle.plugins.DownloadUrl],layout:\"StandaloneLayout\"})};";
 
@@ -25,14 +26,15 @@ final class StringUtil {
     }
 
     static String generateInit(OpenApiEntry entry) {
-        return INIT_JS.replace(MARKER, urlEntry(entry.getURI().toString(), entry.getName()));
+        var array = URLS_PREFIX + urlEntry(entry.getURI().toString(), entry.getName()) + "]";
+        return INIT_JS.replace(MARKER, array);
     }
 
     static String generateInit(OpenApiEntry[] entries) {
         if (entries.length == 0) {
             return EMPTY_URLS;
         }
-        var array = new StringBuilder("urls:[");
+        var array = new StringBuilder(URLS_PREFIX);
         var first = entries[0];
         array.append(urlEntry(first.getURI().toString(), first.getName()));
         for (var i = 1; i < entries.length; ++i) {
@@ -48,7 +50,7 @@ final class StringUtil {
         if (!iterator.hasNext()) {
             return EMPTY_URLS;
         }
-        var array = new StringBuilder("urls:[");
+        var array = new StringBuilder(URLS_PREFIX);
         var first = iterator.next();
         array.append(urlEntry(first.getURI().toString(), first.getName()));
         while (iterator.hasNext()) {
