707b67ed36b377a094777e9f55f211d6ec3e569c
Roman Bakaldin <gbakaldin@gmail.com>
2025-09-02 14:33:26 +0300

    Add javadoс for module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,21 @@
 /**
- * TODO
+ * Provides integration of Swagger UI and OpenAPI document serving
+ * into an Amaya web application.
+ *
+ * <p>This module includes:
+ * <ul>
+ *   <li>{@link io.github.amayaframework.swagger.SwaggerTaskBuilder} –
+ *   a builder for creating Swagger tasks that serve the UI and documents,</li>
+ *   <li>{@link io.github.amayaframework.swagger.SwaggerApplicationConfigurer} –
+ *   application-level configurer for registering Swagger in
+ *   {@link io.github.amayaframework.web.WebApplication},</li>
+ *   <li>{@link io.github.amayaframework.swagger.SwaggerOptions} –
+ *   option keys for configuring Swagger via {@link io.github.amayaframework.options.OptionSet},</li>
+ *   <li>{@link io.github.amayaframework.swagger.OpenApiSource} and related
+ *   classes for representing OpenAPI document sources,</li>
+ *   <li>support for content negotiation and compression using
+ *   {@link io.github.amayaframework.compress.CompressNegotiator}.</li>
+ * </ul>
  */
 module amayaframework.swagger {
     // TODO REMOVE dev deps
